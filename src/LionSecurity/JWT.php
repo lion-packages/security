c361c9d7a109763a3bc72d70519ecc4b55f6ada1
Sergio León <56183278+Sleon4@users.noreply.github.com>
2022-04-03 16:46:56 -0500

    The use of exceptions to decode JWT has been implemented.

--- src/LionSecurity/JWT.php
+++ src/LionSecurity/JWT.php
@@ -2,7 +2,7 @@
 
 namespace LionSecurity;
 
-use Firebase\JWT\{ JWT as FJWT, Key };
+use Firebase\JWT\{ SignatureInvalidException, BeforeValidException, ExpiredException, Key, JWT as FBJWT };
 use LionSecurity\RSA;
 
 class JWT {
@@ -15,7 +15,7 @@ class JWT {
 		RSA::init();
 		$now = strtotime("now");
 
-		return FJWT::encode([
+		return FBJWT::encode([
 			'iss' => $_ENV['SERVER_URL'],
 			'aud' => $_ENV['SERVER_URL_AUD'],
 			"jti" => base64_encode(random_bytes(16)),
@@ -29,9 +29,25 @@ class JWT {
 	public static function decode(string $jwt): object {
 		RSA::init();
 
-		return FJWT::decode(
-			$jwt, new Key(RSA::$public_key, $_ENV['JWT_DEFAULT_MD'])
-		);
+		try {
+			$jwt = FBJWT::decode(
+				$jwt, new Key(RSA::$public_key, $_ENV['JWT_DEFAULT_MD'])
+			);
+
+			return (object) [
+				'status' => "success",
+				'message' => "JWT decoded successfully.",
+				'data' => $jwt->data
+			];
+		} catch (SignatureInvalidException $e) {
+			return (object) ['status' => "error", 'message' => $e->getMessage()];
+		} catch (BeforeValidException $e) {
+			return (object) ['status' => "error", 'message' => $e->getMessage()];
+		} catch (ExpiredException $e) {
+			return (object) ['status' => "error", 'message' => $e->getMessage()];
+		} catch (Exception $e) {
+			return (object) ['status' => "error", 'message' => $e->getMessage()];
+		}
 	}
 
 	public static function getToken(): string {
