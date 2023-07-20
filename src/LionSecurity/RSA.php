a6526104ca39720c4c05eaf70c6ed7fb521d27d2
Sergio Leon <sergioleon4004@hotmail.com>
2023-07-20 10:36:39 -0500

    feat: getter and setter methods have been added to modify rsa values

--- src/LionSecurity/RSA.php
+++ src/LionSecurity/RSA.php
@@ -6,15 +6,17 @@ use \OpenSSLAsymmetricKey;
 
 class RSA {
 
-	public static ?OpenSSLAsymmetricKey $public_key = null;
-	public static ?OpenSSLAsymmetricKey $private_key = null;
-	public static string $url_path = "storage/keys/";
+	private static ?OpenSSLAsymmetricKey $public_key = null;
+	private static ?OpenSSLAsymmetricKey $private_key = null;
 
-	public function __construct() {
+	private static string $url_path = "storage/keys/";
+	private static array $rsa_config = [
+		'config' => '/etc/ssl/openssl.cnf',
+		'private_key_bits' => 2048,
+		'default_md' => 'sha256'
+	];
 
-	}
-
-	public static function init(): void {
+	protected static function init(): void {
 		if (self::$public_key === null) {
 			self::$public_key = openssl_pkey_get_public(
 				file_get_contents(self::$url_path . 'public.key')
@@ -29,47 +31,62 @@ class RSA {
 	}
 
 	public static function createKeys(?string $url_path = null): void {
-		$options = [
-			'config' => $_ENV['RSA_PATH'],
-			'private_key_bits' => (int) $_ENV['RSA_PRIVATE_KEY_BITS'],
-			'default_md' => $_ENV['RSA_DEFAULT_MD']
-		];
-
-		$generate = openssl_pkey_new($options);
-		openssl_pkey_export($generate, $private, null, $options);
+		$generate = openssl_pkey_new(self::$rsa_config);
+		openssl_pkey_export($generate, $private, null, self::$rsa_config);
 		$public = openssl_pkey_get_details($generate);
 
-		if ($url_path != null) {
-			file_put_contents("{$url_path}private.key", $private);
-			file_put_contents("{$url_path}public.key", $public['key']);
-		} else {
-			file_put_contents(self::$url_path . 'private.key', $private);
-			file_put_contents(self::$url_path . 'public.key', $public['key']);
-		}
+		$path_private_key = $url_path === null ? self::$url_path . 'private.key' : "{$url_path}private.key";
+		$path_public_key = $url_path === null ? self::$url_path . 'public.key' : "{$url_path}public.key";
+		file_put_contents($path_private_key, $private);
+		file_put_contents($path_public_key, $public['key']);
 	}
 
-	public static function encode(array $files): object {
+	public static function encode(array $rows): object {
 		self::init();
 		$data_list = [];
 
-		foreach ($files as $key => $file) {
-			openssl_public_encrypt($file, $data, self::$public_key);
+		foreach ($rows as $key => $row) {
+			openssl_public_encrypt($row, $data, self::$public_key);
 			$data_list[$key] = $data;
 		}
 
 		return (object) $data_list;
 	}
 
-	public static function decode(array $files): object {
+	public static function decode(array $rows): object {
 		self::init();
 		$data_list = [];
 
-		foreach ($files as $key => $file) {
-			openssl_private_decrypt($file, $data, self::$private_key);
+		foreach ($rows as $key => $row) {
+			openssl_private_decrypt($row, $data, self::$private_key);
 			$data_list[$key] = $data;
 		}
 
 		return (object) $data_list;
+	}
+
+	public static function getConfig(): array {
+		return self::$rsa_config;
+	}
+
+	public static function setConfig(array $rsa_config): void {
+		self::$rsa_config = $rsa_config;
+	}
+
+	public static function getUrlPath(): string {
+		return self::$url_path;
+	}
+
+	public static function setUrlPath(string $url_path): void {
+		self::$url_path = $url_path;
+	}
+
+	public static function getPublicKey(): ?OpenSSLAsymmetricKey {
+		return self::$public_key;
+	}
+
+	public static function getPrivateKey(): ?OpenSSLAsymmetricKey {
+		return self::$private_key;
 	}
 
 }
