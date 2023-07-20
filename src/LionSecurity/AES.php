2bde06cdb40e4edfb08b44ecf41328480d9519a8
Sergio Leon <sergioleon4004@hotmail.com>
2023-07-20 11:46:46 -0500

    refactor: variables in AES class functions have been renamed

--- src/LionSecurity/AES.php
+++ src/LionSecurity/AES.php
@@ -4,28 +4,24 @@ namespace LionSecurity;
 
 class AES {
 
-	public function __construct() {
-
-	}
-
-	public static function encode(array $files, string $select_key, string $select_iv): object {
+	public static function encode(string $key, string $iv, array $files): object {
 		$data_list = [];
 
 		foreach ($files as $key => $file) {
 			$data_list[$key] = base64_encode(
-				openssl_encrypt($file, $_ENV['AES_METHOD'], md5($_ENV[$select_key]), OPENSSL_RAW_DATA, $_ENV[$select_iv])
+				openssl_encrypt($file, $_ENV['AES_METHOD'], md5($key), OPENSSL_RAW_DATA, $iv)
 			);
 		}
 
 		return (object) $data_list;
 	}
 
-	public static function decode(array $files, string $select_key, string $select_iv): object {
+	public static function decode(string $key, string $iv, array $files): object {
 		$data_list = [];
 
 		foreach ($files as $key => $file) {
 			$data_list[$key] = openssl_decrypt(
-				base64_decode($file), $_ENV['AES_METHOD'], md5($_ENV[$select_key]), OPENSSL_RAW_DATA, $_ENV[$select_iv]
+				base64_decode($file), $_ENV['AES_METHOD'], md5($key), OPENSSL_RAW_DATA, $iv
 			);
 		}
 
