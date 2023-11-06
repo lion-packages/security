c8981e6977e5bad9a236191773d6a8ca006baf3d
Sleon4 <sergioleon4004@hotmail.com>
2023-11-06 09:25:09 -0500

    refactor: JWT class has been refactored

--- src/LionSecurity/JWT.php
+++ src/LionSecurity/JWT.php
@@ -1,96 +1,227 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LionSecurity;
 
-use \Closure;
-use \DomainException;
+use Closure;
+use DomainException;
 use Firebase\JWT\SignatureInvalidException;
 use Firebase\JWT\BeforeValidException;
 use Firebase\JWT\ExpiredException;
 use Firebase\JWT\Key;
 use Firebase\JWT\JWT as FBJWT;
-use \InvalidArgumentException;
+use InvalidArgumentException;
+use LionSecurity\Exceptions\InvalidConfigException;
 use LionSecurity\RSA;
-use \UnexpectedValueException;
+use UnexpectedValueException;
 
 class JWT
 {
-	private static function execute(Closure $execute_function): object
+    const RSA = 'RSA';
+    const AES = 'AES';
+    const METHODS = [self::RSA, self::AES];
+
+    private RSA $rsa;
+    private AES $aes;
+
+    private array|object $values = [];
+    private bool $selectedMethod = self::RSA;
+    private array $configValues = [];
+    private string $jwtServerUrl = 'http://127.0.0.1:8000';
+    private string $jwtServerUrlAud = 'http://127.0.0.1:5173';
+    private int $jwtExp = 3600;
+    private string $jwtDefaultMD = 'RS256';
+
+    public function __construct()
     {
-		try {
-			return $execute_function();
-		} catch (InvalidArgumentException $e) {
-			return (object) ['status' => "error", 'message' => $e->getMessage()];
-		} catch (DomainException $e) {
-			return (object) ['status' => "error", 'message' => $e->getMessage()];
-		} catch (SignatureInvalidException $e) {
-			return (object) ['status' => "error", 'message' => $e->getMessage()];
-		} catch (BeforeValidException $e) {
-			return (object) ['status' => "error", 'message' => $e->getMessage()];
-		} catch (ExpiredException $e) {
-			return (object) ['status' => "error", 'message' => $e->getMessage()];
-		} catch (UnexpectedValueException $e) {
-			return (object) ['status' => "error", 'message' => $e->getMessage()];
-		}
-	}
+        $this->rsa = new RSA();
+        $this->aes = new AES();
+    }
 
-	public static function encode(array $data, int $time = 0): object
+    /**
+     * Clear variables so they have their original value
+     * */
+    private function clean(): void
     {
-		RSA::init();
+        $this->values = [];
+        $this->selectedMethod = 'RSA';
+        $this->configValues = [];
+        $this->jwtServerUrl = 'http://127.0.0.1:8000';
+        $this->jwtServerUrlAud = 'http://127.0.0.1:5173';
+        $this->jwtExp = 3600;
+        $this->jwtDefaultMD = 'RS256';
+    }
 
-		return self::execute(function() use ($data, $time) {
-			$now = strtotime("now");
-
-			return (object) [
-				'status' => "success",
-				'message' => "JWT encodes correctly.",
-				'data' => (object) [
-					'jwt' => FBJWT::encode([
-						'iss' => $_ENV['SERVER_URL'],
-						'aud' => $_ENV['SERVER_URL_AUD'],
-						"jti" => base64_encode(random_bytes(16)),
-						"iat" => $now,
-						"nbf" => $now,
-						'exp' => $now + ($time === 0 ? ((int) $_ENV['JWT_EXP']) : $time),
-						'data' => $data
-					], RSA::getPrivateKey(), $_ENV['JWT_DEFAULT_MD'])
-				]
-			];
-		});
-	}
-
-	public static function decode(?string $jwt): object
+    /**
+     * Define settings for AES encryption
+     * */
+    public function config(string $method, array $config): JWT
     {
-		RSA::init();
+        if (!empty($config['jwtServerUrl'])) {
+            $this->jwtServerUrl = $config['jwtServerUrl'];
+        }
 
-		if ($jwt === 'null' || $jwt === null) {
-			return (object) ['status' => "error", 'message' => "The JWT does not exist."];
-		}
+        if (!empty($config['jwtServerUrlAud'])) {
+            $this->jwtServerUrlAud = $config['jwtServerUrlAud'];
+        }
 
-		return self::execute(function() use ($jwt) {
-			return (object) [
-				'status' => "success",
-				'message' => "JWT decoded successfully.",
-				'data' => (object) [
-					'jwt' => FBJWT::decode(
-						$jwt,
-						new Key(RSA::getPublicKey(), $_ENV['JWT_DEFAULT_MD'])
-					)
-				]
-			];
-		});
-	}
+        if (!empty($config['jwtExp'])) {
+            $this->jwtExp = $config['jwtExp'];
+        }
 
-	public static function get(): string
+        if (!empty($config['jwtDefaultMD'])) {
+            $this->jwtDefaultMD = $config['jwtDefaultMD'];
+        }
+
+        $this->selectedMethod = in_array($method, self::METHODS, true) ? $method : self::RSA;
+        $this->configValues = $config;
+
+        return $this;
+    }
+
+    /**
+     * Run the encryption/decryption process
+     * */
+    private function execute(Closure $executeFunction): void
     {
-		$headers = apache_request_headers();
+        try {
+            $this->values = $executeFunction();
+        } catch (InvalidArgumentException $e) {
+            $this->values = (object) ['status' => 'error', 'message' => $e->getMessage()];
+        } catch (DomainException $e) {
+            $this->values = (object) ['status' => 'error', 'message' => $e->getMessage()];
+        } catch (SignatureInvalidException $e) {
+            $this->values = (object) ['status' => 'error', 'message' => $e->getMessage()];
+        } catch (BeforeValidException $e) {
+            $this->values = (object) ['status' => 'error', 'message' => $e->getMessage()];
+        } catch (ExpiredException $e) {
+            $this->values = (object) ['status' => 'error', 'message' => $e->getMessage()];
+        } catch (UnexpectedValueException $e) {
+            $this->values = (object) ['status' => 'error', 'message' => $e->getMessage()];
+        }
+    }
 
-		if (isset($headers['Authorization'])) {
-			if (preg_match('/Bearer\s(\S+)/', $headers['Authorization'], $matches)) {
-				return $matches[1];
-			}
-		}
+    /**
+     * Encrypt data with defined settings
+     * */
+    public function encode(array $data, int $time = 0, int $bytes = 16): void
+    {
+        $this->execute(function() use ($data, $time, $bytes) {
+            if (empty($config['privateKey'])) {
+                throw new InvalidConfigException('The privateKey has not been defined');
+            }
 
-		return false;
-	}
+            $now = strtotime("now");
+
+            $config = [
+                'iss' => $this->jwtServerUrl,
+                'aud' => $this->jwtServerUrlAud,
+                "jti" => base64_encode(random_bytes($bytes)),
+                "iat" => $now,
+                "nbf" => $now,
+                'exp' => $now + (0 === $time ? ((int) $this->jwtExp) : $time),
+                'data' => $data
+            ];
+
+            return FBJWT::encode($config, $this->configValues['privateKey'], $this->jwtDefaultMD);
+        });
+    }
+
+    /**
+     * Decodes the data with the defined settings
+     * */
+    public function decode(?string $jwt = ''): void
+    {
+        $this->execute(function() use ($jwt) {
+            if (empty($config['publicKey'])) {
+                throw new InvalidConfigException('The publicKey has not been defined');
+            }
+
+            if (in_array($jwt, ['null', null, ''], true)) {
+                return (object) ['status' => "error", 'message' => "The JWT does not exist"];
+            }
+
+            return FBJWT::decode($jwt, new Key($this->configValues['publicKey'], $this->jwtDefaultMD));
+        });
+    }
+
+    /**
+     * Modify the serverUrl to generate the token
+     * */
+    public function jwtServerUrl(string $jwtServerUrl): JWT
+    {
+        $this->jwtServerUrl = $jwtServerUrl;
+
+        return $this;
+    }
+
+    /**
+     * Modify the serverUrlAud to generate the token
+     * */
+    public function jwtServerUrlAud(string $jwtServerUrlAud): JWT
+    {
+        $this->jwtServerUrlAud = $jwtServerUrlAud;
+
+        return $this;
+    }
+
+    /**
+     * Modify the exp to generate the token
+     * */
+    public function jwtExp(int $jwtExp): JWT
+    {
+        $this->jwtExp = $jwtExp;
+
+        return $this;
+    }
+
+    /**
+     * Modify the defaultMD to generate the token
+     * */
+    public function jwtDefaultMD(int $jwtDefaultMD): JWT
+    {
+        $this->jwtDefaultMD = $jwtDefaultMD;
+
+        return $this;
+    }
+
+    /**
+     * Gets the Authorization header token
+     * */
+    public function getJWT(): string|bool
+    {
+        $headers = apache_request_headers();
+
+        if (isset($headers['Authorization'])) {
+            if (preg_match('/Bearer\s(\S+)/', $headers['Authorization'], $matches)) {
+                return $matches[1];
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Converts the list with data to an object
+     * */
+    public function toObject(): JWT
+    {
+        if (gettype($this->values) === 'array') {
+            $this->values = (object) $this->values;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Returns the current array/object with the encrypted/decrypted data
+     * */
+    public function get(): array|object
+    {
+        $values = $this->values;
+        $this->clean();
+
+        return $values;
+    }
 }
