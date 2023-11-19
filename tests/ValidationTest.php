2aa3ed286884a9b1f3f3d755bdf7a961a2f7d6bf
Sleon4 <sergioleon4004@hotmail.com>
2023-11-19 11:49:54 -0500

    test: LionTest has been integrated for testing

--- tests/ValidationTest.php
+++ tests/ValidationTest.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Tests;
 
 use LionSecurity\Validation;
-use PHPUnit\Framework\TestCase;
+use LionTest\Test;
 
-class ValidationTest extends TestCase
+class ValidationTest extends Test
 {
     private Validation $validation;
 
