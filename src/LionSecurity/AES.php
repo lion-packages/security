5d233b14588c782613825a0d143ea7d64906522c
Sleon4 <sergioleon4004@hotmail.com>
2024-02-22 19:10:10 -0500

    fix: AES encryption error has been fixed

--- src/LionSecurity/AES.php
+++ src/LionSecurity/AES.php
@@ -4,95 +4,35 @@ declare(strict_types=1);
 
 namespace Lion\Security;
 
-use Lion\Security\Exceptions\InvalidConfigException;
+use Lion\Security\Exceptions\AESException;
 use Lion\Security\Interfaces\ConfigInterface;
 use Lion\Security\Interfaces\EncryptionInterface;
 use Lion\Security\Interfaces\ObjectInterface;
 
+/**
+ * It allows you to generate the configuration required for AES encryption and
+ * decryption, it has methods that allow you to encrypt and decrypt data
+ * with AES
+ *
+ * @package Lion\Security
+ */
 class AES implements ConfigInterface, EncryptionInterface, ObjectInterface
 {
-    const AES_128_CBC = 'aes-128-cbc';
-    const AES_128_CBC_CTS = 'aes-128-cbc-cts';
-    const AES_128_CBC_HMAC_SHA1 = 'aes-128-cbc-hmac-sha1';
-    const AES_128_CBC_HMAC_SHA256 = 'aes-128-cbc-hmac-sha256';
-    const AES_128_CCM = 'aes-128-ccm';
-    const AES_128_CFB = 'aes-128-cfb';
-    const AES_128_CFB1 = 'aes-128-cfb1';
-    const AES_128_CFB8 = 'aes-128-cfb8';
-    const AES_128_CTR = 'aes-128-ctr';
-    const AES_128_ECB = 'aes-128-ecb';
-    const AES_128_GCM = 'aes-128-gcm';
-    const AES_128_OCB = 'aes-128-ocb';
-    const AES_128_OFB = 'aes-128-ofb';
-    const AES_128_SIV = 'aes-128-siv';
-    const AES_128_WRAP = 'aes-128-wrap';
-    const AES_128_WRAP_INV = 'aes-128-wrap-inv';
-    const AES_128_WRAP_PAD = 'aes-128-wrap-pad';
-    const AES_128_WRAP_PAD_INV = 'aes-128-wrap-pad-inv';
-    const AES_128_XTS = 'aes-128-xts';
-    const AES_192_CBC = 'aes-192-cbc';
-    const AES_192_CBC_CTS = 'aes-192-cbc-cts';
-    const AES_192_CCM = 'aes-192-ccm';
-    const AES_192_CFB = 'aes-192-cfb';
-    const AES_192_CFB1 = 'aes-192-cfb1';
-    const AES_192_CFB8 = 'aes-192-cfb8';
-    const AES_192_CTR = 'aes-192-ctr';
-    const AES_192_ECB = 'aes-192-ecb';
-    const AES_192_GCM = 'aes-192-gcm';
-    const AES_192_OCB = 'aes-192-ocb';
-    const AES_192_OFB = 'aes-192-ofb';
-    const AES_192_SIV = 'aes-192-siv';
-    const AES_192_WRAP = 'aes-192-wrap';
-    const AES_192_WRAP_INV = 'aes-192-wrap-inv';
-    const AES_192_WRAP_PAD = 'aes-192-wrap-pad';
-    const AES_192_WRAP_PAD_INV = 'aes-192-wrap-pad-inv';
+    /**
+     * @const AES_256_CBC [It's a robust encryption method with a 256-bit key
+     * size and uses Cipher Block Chaining (CBC) mode]
+     */
     const AES_256_CBC = 'aes-256-cbc';
-    const AES_256_CBC_CTS = 'aes-256-cbc-cts';
-    const AES_256_CBC_HMAC_SHA1 = 'aes-256-cbc-hmac-sha1';
-    const AES_256_CBC_HMAC_SHA256 = 'aes-256-cbc-hmac-sha256';
-    const AES_256_CCM = 'aes-256-ccm';
-    const AES_256_CFB = 'aes-256-cfb';
-    const AES_256_CFB1 = 'aes-256-cfb1';
-    const AES_256_CFB8 = 'aes-256-cfb8';
-    const AES_256_CTR = 'aes-256-ctr';
-    const AES_256_ECB = 'aes-256-ecb';
-    const AES_256_GCM = 'aes-256-gcm';
-    const AES_256_OCB = 'aes-256-ocb';
-    const AES_256_OFB = 'aes-256-ofb';
-    const AES_256_SIV = 'aes-256-siv';
-    const AES_256_WRAP = 'aes-256-wrap';
-    const AES_256_WRAP_INV = 'aes-256-wrap-inv';
-    const AES_256_WRAP_PAD = 'aes-256-wrap-pad';
-    const AES_256_WRAP_PAD_INV = 'aes-256-wrap-pad-inv';
-    const AES_256_XTS = 'aes-256-xts';
 
     private array|object $values = [];
-    private string $method = '';
-    private string $key = '';
-    private string $iv = '';
+    private array $config = [];
 
     /**
      * {@inheritdoc}
      * */
-    public function config(array $config): AES
+    public function config(array|object $config): AES
     {
-        if (empty($config['key'])) {
-            throw new InvalidConfigException('The key has not been defined');
-        } else {
-            $this->key = $config['key'];
-        }
-
-        if (empty($config['iv'])) {
-            throw new InvalidConfigException('The Iv has not been defined');
-        } else {
-            $this->iv = $config['iv'];
-        }
-
-        if (empty($config['method']) && '' === $this->method) {
-            $this->method = self::AES_256_CBC;
-        } else {
-            $this->method = $config['method'];
-        }
+        $this->config = (array) $config;
 
         return $this;
     }
@@ -113,7 +53,18 @@ class AES implements ConfigInterface, EncryptionInterface, ObjectInterface
      * */
     public function encode(string $key, string $value): AES
     {
-        $encrypt = openssl_encrypt($value, $this->method, md5($this->key), OPENSSL_RAW_DATA, $this->iv);
+        $encrypt = openssl_encrypt(
+            $value,
+            $this->config['method'],
+            hex2bin($this->config['key']),
+            OPENSSL_RAW_DATA,
+            hex2bin($this->config['iv'])
+        );
+
+        if (!$encrypt) {
+            throw new AESException(openssl_error_string());
+        }
+
         $this->values[$key] = base64_encode($encrypt);
 
         return $this;
@@ -127,10 +78,10 @@ class AES implements ConfigInterface, EncryptionInterface, ObjectInterface
         foreach ($rows as $key => $row) {
             $this->values[$key] = openssl_decrypt(
                 base64_decode($row),
-                $this->method,
-                md5($this->key),
+                $this->config['method'],
+                hex2bin($this->config['key']),
                 OPENSSL_RAW_DATA,
-                $this->iv
+                hex2bin($this->config['iv'])
             );
         }
 
@@ -157,9 +108,7 @@ class AES implements ConfigInterface, EncryptionInterface, ObjectInterface
     private function clean(): void
     {
         $this->values = [];
-        $this->method = '';
-        $this->key = '';
-        $this->iv = '';
+        $this->config = [];
     }
 
     /**
@@ -167,65 +116,12 @@ class AES implements ConfigInterface, EncryptionInterface, ObjectInterface
      *
      * @param  string $method [AES algorithm type]
      *
-     * @return int|false
+     * @return array|bool|int
      */
-    public function cipherKeyLength(string $method): int|false
+    public function cipherKeyLength(string $method): array|bool|int
     {
         $length = match (trim(strtolower($method))) {
-            self::AES_128_CBC => 16,
-            self::AES_128_CBC_CTS => 16,
-            self::AES_128_CBC_HMAC_SHA1 => 16,
-            self::AES_128_CBC_HMAC_SHA256 => 16,
-            self::AES_128_CCM => 16,
-            self::AES_128_CFB => 16,
-            self::AES_128_CFB1 => 16,
-            self::AES_128_CFB8 => 16,
-            self::AES_128_CTR => 16,
-            self::AES_128_ECB => 16,
-            self::AES_128_GCM => 16,
-            self::AES_128_OCB => 16,
-            self::AES_128_OFB => 16,
-            self::AES_128_SIV => 16,
-            self::AES_128_WRAP => 16,
-            self::AES_128_WRAP_INV => 16,
-            self::AES_128_WRAP_PAD => 16,
-            self::AES_128_WRAP_PAD_INV => 16,
-            self::AES_128_XTS => 16,
-            self::AES_192_CBC => 24,
-            self::AES_192_CBC_CTS => 24,
-            self::AES_192_CCM => 24,
-            self::AES_192_CFB => 24,
-            self::AES_192_CFB1 => 24,
-            self::AES_192_CFB8 => 24,
-            self::AES_192_CTR => 24,
-            self::AES_192_ECB => 24,
-            self::AES_192_GCM => 24,
-            self::AES_192_OCB => 24,
-            self::AES_192_OFB => 24,
-            self::AES_192_SIV => 24,
-            self::AES_192_WRAP => 24,
-            self::AES_192_WRAP_INV => 24,
-            self::AES_192_WRAP_PAD => 24,
-            self::AES_192_WRAP_PAD_INV => 24,
             self::AES_256_CBC => 32,
-            self::AES_256_CBC_CTS => 32,
-            self::AES_256_CBC_HMAC_SHA1 => 32,
-            self::AES_256_CBC_HMAC_SHA256 => 32,
-            self::AES_256_CCM => 32,
-            self::AES_256_CFB => 32,
-            self::AES_256_CFB1 => 32,
-            self::AES_256_CFB8 => 32,
-            self::AES_256_CTR => 32,
-            self::AES_256_ECB => 32,
-            self::AES_256_GCM => 32,
-            self::AES_256_OCB => 32,
-            self::AES_256_OFB => 32,
-            self::AES_256_SIV => 32,
-            self::AES_256_WRAP => 32,
-            self::AES_256_WRAP_INV => 32,
-            self::AES_256_WRAP_PAD => 32,
-            self::AES_256_WRAP_PAD_INV => 32,
-            self::AES_256_XTS => 32,
             default => false
         };
 
@@ -241,55 +137,13 @@ class AES implements ConfigInterface, EncryptionInterface, ObjectInterface
      */
     public function create(string $method): AES
     {
-        $bits = $this->cipherKeyLength($method);
+        $cipherKey = $this->cipherKeyLength($method);
 
         $this->values = [
-            'bits' => $bits,
-            'key' => bin2hex(random_bytes($bits / 2)),
-            'iv' => bin2hex(random_bytes($bits / 2))
+            'key' => bin2hex(openssl_random_pseudo_bytes($cipherKey)),
+            'iv' => bin2hex(openssl_random_pseudo_bytes(16)),
+            'method' => $method
         ];
-
-        return $this;
-    }
-
-    /**
-     * Defines the encryption method
-     *
-     * @param  string $method [AES algorithm type]
-     *
-     * @return AES
-     */
-    public function method(string $method): AES
-    {
-        $this->method = $method;
-
-        return $this;
-    }
-
-    /**
-     * Defines the encryption key
-     *
-     * @param  string $key [AES encryption KEY]
-     *
-     * @return AES
-     */
-    public function key(string $key): AES
-    {
-        $this->key = $key;
-
-        return $this;
-    }
-
-    /**
-     * Defines the encryption iv
-     *
-     * @param  string $iv [AES encryption IV]
-     *
-     * @return AES
-     */
-    public function iv(string $iv): AES
-    {
-        $this->iv = $iv;
 
         return $this;
     }
--- tests/AESTest.php
+++ tests/AESTest.php
@@ -5,19 +5,17 @@ declare(strict_types=1);
 namespace Tests;
 
 use Lion\Security\AES;
-use Lion\Security\Exceptions\InvalidConfigException;
+use Lion\Security\Interfaces\ConfigInterface;
+use Lion\Security\Interfaces\EncryptionInterface;
+use Lion\Security\Interfaces\ObjectInterface;
 use Lion\Test\Test;
 use ReflectionClass;
 use stdClass;
-use Tests\Support\Providers\AESEncryptionMethodProvider;
+use Tests\Providers\AESEncryptionMethodProvider;
 
 class AESTest extends Test
 {
     use AESEncryptionMethodProvider;
-
-    const KEY = '0123456789sleon4';
-    const IV = 'sleon40123456789';
-    const CONFIG = ['key' => self::KEY, 'iv' => self::IV, 'method' => AES::AES_256_CBC];
 
     private AES $aes;
     private ReflectionClass $reflectionClass;
@@ -25,15 +23,101 @@ class AESTest extends Test
     protected function setUp(): void
     {
         $this->aes = new AES();
-        $this->reflectionClass = new ReflectionClass($this->aes);
+
+        $this->initReflection($this->aes);
     }
 
-    public function testCipherKeyLength(): void
+    /**
+     * @dataProvider AESEncryptionMethodProvider
+     */
+    public function testConfig(string $method): void
     {
-        $lenght = $this->aes->cipherKeyLength(AES::AES_256_CBC);
+        $config = $this->aes->create($method)->get();
+
+        $this->assertInstances($this->aes->config($config), [
+            AES::class,
+            ConfigInterface::class,
+            EncryptionInterface::class,
+            ObjectInterface::class
+        ]);
+
+        $this->assertSame($config, $this->getPrivateProperty('config'));
+    }
+
+    public function testGet(): void
+    {
+        $config = $this->aes->create(AES::AES_256_CBC)->get();
+        $encode = $this->aes->config($config)->encode('user_name', 'Sleon')->get();
+
+        $this->assertIsArray($encode);
+        $this->assertArrayHasKey('user_name', $encode);
+    }
+
+    /**
+     * @dataProvider AESEncryptionMethodProvider
+     */
+    public function testEncode(string $method): void
+    {
+        $config = $this->aes->create($method)->get();
+
+        $this->assertInstances($this->aes->config($config)->encode('user_name', 'Sleon'), [
+            AES::class,
+            ConfigInterface::class,
+            EncryptionInterface::class,
+            ObjectInterface::class
+        ]);
+
+        $values = $this->aes->get();
+
+        $this->assertArrayHasKey('user_name', $values);
+        $this->assertIsString($values['user_name']);
+    }
+
+    public function testDecode(): void
+    {
+        $config = $this->aes->create(AES::AES_256_CBC)->get();
+
+        $key1 = 'key1';
+        $value1 = 'encoded_value_1';
+
+        $key2 = 'key2';
+        $value2 = 'encoded_value_2';
+
+        $encode = $this->aes->config($config)->encode($key1, $value1)->encode($key2, $value2)->get();
+        $decode = $this->aes->config($config)->decode($encode)->get();
+
+        $this->assertArrayHasKey($key1, $decode);
+        $this->assertArrayHasKey($key2, $decode);
+        $this->assertSame($value1, $decode[$key1]);
+        $this->assertSame($value2, $decode[$key2]);
+    }
+
+    public function testToObject(): void
+    {
+        $config = $this->aes->create(AES::AES_256_CBC)->get();
+        $encode = $this->aes->config($config)->encode('user_name', 'Sleon')->toObject()->get();
+
+        $this->assertInstanceOf(stdClass::class, $encode);
+        $this->assertObjectHasProperty('user_name', $encode);
+    }
+
+    public function testClean(): void
+    {
+        $this->getPrivateMethod('clean');
+
+        $this->assertSame([], $this->getPrivateProperty('values'));
+        $this->assertSame([], $this->getPrivateProperty('config'));
+    }
+
+    /**
+     * @dataProvider AESEncryptionMethodProvider
+     * */
+    public function testCipherKeyLength(string $method, int $bits): void
+    {
+        $lenght = $this->aes->cipherKeyLength($method);
 
         $this->assertIsInt($lenght);
-        $this->assertSame(32, $lenght);
+        $this->assertSame($bits, $lenght);
     }
 
     public function testCipherKeyLengthNotExist(): void
@@ -44,130 +128,16 @@ class AESTest extends Test
         $this->assertFalse($lenght);
     }
 
-    public function testConfig(): void
-    {
-        $this->assertInstanceOf(AES::class, $this->aes->config(self::CONFIG));
-
-        $keyProperty = $this->reflectionClass->getProperty('key');
-        $keyProperty->setAccessible(true);
-
-        $ivProperty = $this->reflectionClass->getProperty('iv');
-        $ivProperty->setAccessible(true);
-
-        $methodProperty = $this->reflectionClass->getProperty('method');
-        $methodProperty->setAccessible(true);
-
-        $this->assertSame(self::KEY, $keyProperty->getValue($this->aes));
-        $this->assertSame(self::IV, $ivProperty->getValue($this->aes));
-        $this->assertSame(AES::AES_256_CBC, $methodProperty->getValue($this->aes));
-    }
-
-    public function testConfigWithMissingKey(): void
-    {
-        $this->expectException(InvalidConfigException::class);
-
-        $this->aes->config(['iv' => self::IV, 'method' => AES::AES_256_CBC]);
-    }
-
-    public function testConfigWithMissingIv(): void
-    {
-        $this->expectException(InvalidConfigException::class);
-
-        $this->aes->config(['key' => self::KEY, 'method' => AES::AES_256_CBC]);
-    }
-
-    public function testConfigWithEmptyMethod(): void
-    {
-        $this->aes->config(['key' => self::KEY, 'iv' => self::IV, 'method' => '']);
-        $methodProperty = $this->reflectionClass->getProperty('method');
-        $methodProperty->setAccessible(true);
-
-        $this->assertSame(AES::AES_256_CBC, $methodProperty->getValue($this->aes));
-    }
-
     /**
      * @dataProvider AESEncryptionMethodProvider
      */
     public function testCreate(string $method, int $bits): void
     {
-        $values = $this->aes->create($method)->get();
+        $config = $this->aes->create($method)->get();
 
-        $this->assertArrayHasKey('key', $values);
-        $this->assertArrayHasKey('iv', $values);
-        $this->assertArrayHasKey('bits', $values);
-        $this->assertSame($bits, $values['bits']);
-    }
-
-    public function testMethod(): void
-    {
-        $methodProperty = $this->reflectionClass->getProperty('method');
-        $methodProperty->setAccessible(true);
-
-        $this->assertInstanceOf(AES::class, $this->aes->method(AES::AES_256_CBC));
-        $this->assertSame(AES::AES_256_CBC, $methodProperty->getValue($this->aes));
-    }
-
-    public function testKey(): void
-    {
-        $keyProperty = $this->reflectionClass->getProperty('key');
-        $keyProperty->setAccessible(true);
-
-        $this->assertInstanceOf(AES::class, $this->aes->key(self::KEY));
-        $this->assertSame(self::KEY, $keyProperty->getValue($this->aes));
-    }
-
-    public function testIv(): void
-    {
-        $keyProperty = $this->reflectionClass->getProperty('iv');
-        $keyProperty->setAccessible(true);
-
-        $this->assertInstanceOf(AES::class, $this->aes->iv(self::IV));
-        $this->assertSame(self::IV, $keyProperty->getValue($this->aes));
-    }
-
-    public function testEncode(): void
-    {
-        $this->aes->config(self::CONFIG);
-        $valuesProperty = $this->reflectionClass->getProperty('values');
-        $valuesProperty->setAccessible(true);
-
-        $this->assertInstanceOf(AES::class, $this->aes->encode('user_name', 'Sleon'));
-
-        $values = $this->aes->get();
-
-        $this->assertArrayHasKey('user_name', $values);
-        $this->assertIsString($values['user_name']);
-    }
-
-    public function testDecode()
-    {
-        $key1 = 'key1';
-        $key2 = 'key2';
-        $value1 = 'encoded_value_1';
-        $value2 = 'encoded_value_2';
-
-        $encode = $this->aes->config(self::CONFIG)->encode($key1, $value1)->encode($key2, $value2)->get();
-        $decode = $this->aes->config(self::CONFIG)->decode($encode)->get();
-
-        $this->assertArrayHasKey($key1, $decode);
-        $this->assertArrayHasKey($key2, $decode);
-        $this->assertSame($value1, $decode[$key1]);
-        $this->assertSame($value2, $decode[$key2]);
-    }
-
-    public function testToObject(): void
-    {
-        $encode = $this->aes->config(self::CONFIG)->encode('user_name', 'Sleon')->toObject()->get();
-
-        $this->assertInstanceOf(stdClass::class, $encode);
-        $this->assertObjectHasProperty('user_name', $encode);
-    }
-
-    public function testGet(): void
-    {
-        $encode = $this->aes->config(self::CONFIG)->encode('user_name', 'Sleon')->get();
-
-        $this->assertIsArray($encode);
-        $this->assertArrayHasKey('user_name', $encode);
+        $this->assertArrayHasKey('key', $config);
+        $this->assertArrayHasKey('iv', $config);
+        $this->assertArrayHasKey('method', $config);
+        $this->assertSame($method, $config['method']);
     }
 }
